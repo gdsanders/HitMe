b5b88b7a92fd4e30d9c416a1f6792d8820dfadff
gdsanders <gd@digitalequity.com>
2015-10-27 23:16:14 -0500

    created closure in UIAlertAction
    Moved startNewRound() and updateLabels()

--- HitMe/ViewController.swift
+++ HitMe/ViewController.swift
@@ -72,13 +72,14 @@ class ViewController: UIViewController {
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         
-        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+            self.startNewRound()
+            self.updateLabels()})
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
-        startNewRound()
-        updateLabels()
+        
     }
     
     
